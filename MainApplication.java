306daa57a936fa88d8c1c37b6a8d8e01047a1097
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-8.local>
2019-02-10 01:20:23 -0500

    Completed working consoles for Basic and Animal

--- MainApplication.java
+++ MainApplication.java
@@ -11,24 +11,19 @@ public class MainApplication {
     public static void main(String[] args) {
         
         String firstPrompt = "Would you like to use the 'Basic', 'Scientific', or 'Mystery' Calculator?";
-        String basicPrompt = "You chose basic";
-        String scientificPrompt = "You chose scientific";
-        String mysteryPrompt = "You chose Mystery";
-        String stringPrompt = "What would you like to add together?";
-        
+
         BasicConsole bc = new BasicConsole();
         String userChoice = bc.getStringInput(firstPrompt);
         
         if(userChoice.equals("Basic")){
-            
-            bc.getStringInput(stringPrompt);
+            bc.basicMain();
         }else if(userChoice.equals("Scientific")) {
             ScientificConsole sc = new ScientificConsole();
-            sc.getStringInput(stringPrompt);
+            //sc.scienticMain();
         }
         else{
             AnimalConsole ac = new AnimalConsole();
-            ac.getStringInput(stringPrompt);
+            ac.animalMain();
         }     
         
     }
